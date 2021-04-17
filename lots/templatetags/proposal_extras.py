e365321e7a3c55a8dcac6d77f7e528c6e0502f8b
achan <achan8642@gmail.com>
2021-04-17 12:19:32 -0700

    changed summarizeProposal query to only ask for lots; moved MV API call to separate template filters in proposal_extras

--- lots/templatetags/proposal_extras.py
+++ lots/templatetags/proposal_extras.py
@@ -1,4 +1,7 @@
+from django.db.models import Sum, F
 from django import template
+from datetime import date
+import yfinance as yf
 
 register = template.Library()
 
@@ -6,3 +9,30 @@ register = template.Library()
 def getType(something):
     print(type(something))
     return(type(something))
+
+@register.filter
+def tmvLabel(title):
+    today = date.today().strftime("%Y-%m-%d")
+    return f"Total Market Value as of {today}"
+
+@register.filter
+def getTotalUnits(setOfLots):
+    return list(setOfLots.aggregate(
+        totalUnits=Sum('units')
+        ).values())[0]
+
+@register.filter
+def getTotalAvailableUnits(setOfLots):
+    return list(setOfLots.aggregate(
+        totalUnits=Sum('unitsAvailable')
+        ).values())[0]
+
+@register.filter
+def getTotalMV(setOfLots):
+    today = date.today().strftime("%Y-%m-%d")
+    closingPrice = yf.download(
+        [setOfLots.first().referencedLot.holding.security.ticker],
+        today)['Adj Close'][0]
+    setOfLots = setOfLots.annotate(mv=F('units') * closingPrice)
+    return list(setOfLots.aggregate(totalMV=Sum('mv')).values())[0]
+    
