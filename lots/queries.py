d1ee9448519c7858ffb06bda82fe51636913dcad
achan <achan8642@gmail.com>
2021-04-16 17:22:24 -0700

    adding yfinance api interaction so that query getDraftTotals retrieves the total market value of selected lots and units

--- lots/queries.py
+++ lots/queries.py
@@ -1,5 +1,6 @@
-from django.db.models import Sum, F, Value, FloatField
+from django.db.models import Sum, F, Value, FloatField, ExpressionWrapper, CharField
 from .models import *
+import yfinance as yf
 
 def summarizeProposal(proposal):
     summaryDict = {
@@ -16,7 +17,7 @@ def getDraftLotCPS(proposal):
         for draftAccount in draftPortfolio.draftAccounts.all():
             returnDict[draftPortfolio, draftAccount] = []
             for draftHolding in draftAccount.draftHoldings.all():
-                draftLots = draftHolding.draftTaxLots.annotate(cps=F("referencedLot__totalFederalCost")/F("referencedLot__units"),
+                draftLots = draftHolding.draftTaxLots.annotate(cps=ExpressionWrapper(F("referencedLot__totalFederalCost")/F("referencedLot__units"),output_field=FloatField()),
                 unitsAvailable=F("referencedLot__units"), totalFederalCost=F("referencedLot__totalFederalCost"))
                 
                 returnDict[draftPortfolio, draftAccount].append(draftLots)
@@ -29,6 +30,10 @@ def getDraftTotals(proposal):
         for draftAccount in draftPortfolio.draftAccounts.all():
             for draftHolding in draftAccount.draftHoldings.all():
                 totalUnits = draftHolding.draftTaxLots.aggregate(totalUnits=Sum('units'))
-                totalsDict[draftPortfolio.id, draftAccount.id, draftHolding.security.ticker] = totalUnits
+
+                closingPrice = yf.download([draftHolding.security.ticker], '2021-4-15')['Adj Close'][0]
+                lotsWithMV = draftHolding.draftTaxLots.annotate(mv=F('units') * closingPrice)
+                totalMV = lotsWithMV.aggregate(totalMV=Sum('mv'))
+                totalsDict[draftPortfolio.id, draftAccount.id, draftHolding.security.ticker] = (totalUnits, totalMV)
 
     return totalsDict
