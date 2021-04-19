17a7acbae7416bba53413747332de466280b1979
achan <achan8642@gmail.com>
2021-04-19 15:24:59 -0700

    fixing math in cost per share calculation in getDraftLotCPS. Was rounding off values instead of providing answer in a decimal format

--- lots/queries.py
+++ lots/queries.py
@@ -1,11 +1,11 @@
-from django.db.models import Sum, F, Value, FloatField, ExpressionWrapper, CharField
+from django.db.models import Sum, F, Value, DecimalField, ExpressionWrapper, CharField
 from .models import *
-import yfinance as yf
+
 
 def summarizeProposal(proposal):
     summaryDict = {
         'lots': getDraftLotCPS(proposal),
-        'totals': getDraftTotals(proposal),
+        # 'totals': getDraftTotals(proposal),
     }
     # print(summaryDict)
     return summaryDict
@@ -17,23 +17,27 @@ def getDraftLotCPS(proposal):
         for draftAccount in draftPortfolio.draftAccounts.all():
             returnDict[draftPortfolio, draftAccount] = []
             for draftHolding in draftAccount.draftHoldings.all():
-                draftLots = draftHolding.draftTaxLots.annotate(cps=ExpressionWrapper(F("referencedLot__totalFederalCost")/F("referencedLot__units"),output_field=FloatField()),
-                unitsAvailable=F("referencedLot__units"), totalFederalCost=F("referencedLot__totalFederalCost"))
-                
+                draftLots = draftHolding.draftTaxLots.annotate(
+                    cps=ExpressionWrapper(
+                        (F("referencedLot__totalFederalCost") * 1.0) / (F("referencedLot__units") * 1.0),
+                        output_field=DecimalField(decimal_places=4)),
+                    unitsAvailable=F("referencedLot__units"), 
+                    totalFederalCost=F("referencedLot__totalFederalCost"))
+                print(draftLots.values('cps'))
                 returnDict[draftPortfolio, draftAccount].append(draftLots)
     return returnDict
 
-def getDraftTotals(proposal):
-    totalsDict = {}
+# def getDraftTotals(proposal):
+#     totalsDict = {}
 
-    for draftPortfolio in proposal.draftPortfolios.all():
-        for draftAccount in draftPortfolio.draftAccounts.all():
-            for draftHolding in draftAccount.draftHoldings.all():
-                totalUnits = draftHolding.draftTaxLots.aggregate(totalUnits=Sum('units'))
+#     for draftPortfolio in proposal.draftPortfolios.all():
+#         for draftAccount in draftPortfolio.draftAccounts.all():
+#             for draftHolding in draftAccount.draftHoldings.all():
+#                 totalUnits = draftHolding.draftTaxLots.aggregate(totalUnits=Sum('units'))
 
-                closingPrice = yf.download([draftHolding.security.ticker], '2021-4-15')['Adj Close'][0]
-                lotsWithMV = draftHolding.draftTaxLots.annotate(mv=F('units') * closingPrice)
-                totalMV = lotsWithMV.aggregate(totalMV=Sum('mv'))
-                totalsDict[draftPortfolio.id, draftAccount.id, draftHolding.security.ticker] = (totalUnits, totalMV)
+#                 closingPrice = yf.download([draftHolding.security.ticker], '2021-4-15')['Adj Close'][0]
+#                 lotsWithMV = draftHolding.draftTaxLots.annotate(mv=F('units') * closingPrice)
+#                 totalMV = lotsWithMV.aggregate(totalMV=Sum('mv'))
+#                 totalsDict[draftPortfolio.id, draftAccount.id, draftHolding.security.ticker] = (totalUnits, totalMV)
 
-    return totalsDict
+#     return totalsDict
