422e9d3ab764d8aeb4931fe08021061973c39605
achan <achan8642@gmail.com>
2021-04-27 16:16:39 -0700

    re-did expandRow function in proposalTable.js so that the breakdown rows show with style.display='table-row'

--- lots/static/js/proposalTable.js
+++ lots/static/js/proposalTable.js
@@ -1,29 +1,22 @@
 
-const expandRow = (e) =>{
-    let b = e.target;
-    let siblingElements = b.parentElement.parentElement.children;
-    let tableLotHeader;
-    let tableLots = [];
-    console.log(siblingElements)
-    for (let i =0; i<siblingElements.length; i++){
-        if (siblingElements[i].className == "tableLotHeader"){
-            tableLotHeader = siblingElements[i]; // find tableLotHeader 
+const expandRow = (e) => {
+    // find the button and use it to find the totalRow element
+    let totalRow = e.target.parentElement.parentElement;
+    // change the styles of the next rows until you reach the next
+    // button
+    let nextSibling = totalRow.nextElementSibling;
+    while (nextSibling.className === 'tableLotHeader' ||
+        nextSibling.className === 'tableLot') {
+            console.log(nextSibling.className)
+        if (nextSibling.style.display === "table-row") {
+            nextSibling.style.display = "none";
+        } else {
+            nextSibling.style.display = "table-row";
         }
-        // find the tableLots and add them to the relevant array
-        if (siblingElements[i].className == "tableLot"){
-            tableLots.push(siblingElements[i]);
-        }
-    }
-    if (tableLotHeader.style.display === "block"){
-        tableLots.forEach(item => {
-            item.style.display ="none";
-        })
-    } else{
-        tableLots.forEach(item => {
-            item.style.display ="block";
-        })
+        nextSibling = nextSibling.nextElementSibling;
     }
 }
+
 document.querySelectorAll('.rowExpandButton').forEach(item => {
     item.addEventListener("click", expandRow);
 })
